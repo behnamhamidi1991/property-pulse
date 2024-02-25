8dc6797fae7ad7192ec7c07f8e5b8baaafe283df
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-25 22:13:26 +0330

    Done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -227,12 +227,13 @@ const Navbar = () => {
                 Add Property
               </Link>
             )}
-
-            <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-              <i className="fa-brands fa-google mr-2"></i>
-              <FaGoogle className="text-white mr-2" />
-              <span>Login or Register</span>
-            </button>
+            {!isLoggedIn && (
+              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
+                <i className="fa-brands fa-google mr-2"></i>
+                <FaGoogle className="text-white mr-2" />
+                <span>Login or Register</span>
+              </button>
+            )}
           </div>
         </div>
       )}
