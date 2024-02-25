15ef692935d23d2c0cb7e751e527c84a43d0b0b3
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-25 19:59:38 +0330

    Commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">Homepage</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolorum
+        facere reprehenderit quas. Facilis iste est enim illo ipsa commodi
+        provident quia, maiores sed iure iusto at culpa dolorem? Explicabo?
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
