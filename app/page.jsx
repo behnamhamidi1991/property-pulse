7d0b7b390095012c84576a623ffd057a95f54a0a
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-26 08:33:51 +0330

    Create 404

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@ import Hero from "@/components/Hero";
 import Link from "next/link";
 import React from "react";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </div>
   );
 };
