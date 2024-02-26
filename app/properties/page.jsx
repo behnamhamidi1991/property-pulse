bc719e5d48ef26132bbd32d69103112b12d6aeb0
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-26 07:21:55 +0330

    Commit

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import properties from "@/properties.json";
 
 const PropertiesPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Welcome</h1>
-      <Link href="/">Go Home</Link>
-    </div>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.map((property) => (
+            <div>{property.name}</div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
