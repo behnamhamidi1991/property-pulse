916cdff9e514ba82e236402e9855d929ba9ca60b
behnamhamidi1991 <eslp91@hotmail.com>
2024-03-01 12:12:09 +0330

    Done

---
+++ app/properties/[...id]/edit/page.jsx
@@ -0,0 +1,12 @@
+import PropertyEditForm from "@/components/PropertyEditForm";
+import React from "react";
+
+const PropertyEditPage = () => {
+  return (
+    <div>
+      <PropertyEditForm />
+    </div>
+  );
+};
+
+export default PropertyEditPage;
