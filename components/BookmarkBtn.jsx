f649a3a79a1ca749cc723e0035a5d4dce04f1070
behnamhamidi1991 <eslp91@hotmail.com>
2024-03-03 07:15:25 +0330

    Commit

--- components/BookmarkBtn.jsx
+++ components/BookmarkBtn.jsx
@@ -9,9 +9,37 @@ const BookmarkBtn = ({ property }) => {
   const userId = session?.user?.id;
 
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkBookmarkStatus = async () => {
+      try {
+        const res = await fetch("/api/bookmarks/check", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            propertyId: property._id,
+          }),
+        });
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setIsBookmarked(data.isBookmarked);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkBookmarkStatus();
+  }, [property._id, userId]);
 
   const handleClick = async () => {
     if (!userId) {
+      setLoading(false);
       toast.error("You need to sign in to bookmark a property");
       return;
     }
@@ -38,7 +66,16 @@ const BookmarkBtn = ({ property }) => {
     }
   };
 
-  return (
+  if (loading) return <p className="text-center">Loading ...</p>;
+
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className=" mr-2" /> Remove Bookmark
+    </button>
+  ) : (
     <button
       onClick={handleClick}
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
